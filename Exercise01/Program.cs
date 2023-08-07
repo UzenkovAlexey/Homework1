2df3e321ed3d758d1537505ad206822e171e3d22
UzenLe <Uzenkovlesha@mail.ru>
2023-08-07 21:29:49 +0300

    Добавил опртделение двухзначго числа1

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -9,14 +9,18 @@
 Console.Write("Введите трёхзначное число: ");
  int number = Convert.ToInt32(Console.ReadLine());
 
-if (number >= 100 && number <=1000) 
+if (number >= 10 && number <=100) 
+{
+  Console.WriteLine($"{number} Третьей цифры нет");
+}
+if   (number >= 100 && number <=1000) 
 {
   string Num = Convert.ToString(number);
   Console.WriteLine($" {number} -- "+ Num[1]);
 }
 else 
 {
-  Console.WriteLine("Число не трёхзначное");
+  Console.WriteLine($"{number} Число не трёхзначное");
 }
 
 
